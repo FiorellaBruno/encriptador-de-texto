49786efa3fc3625fe64c5a3671aea804ed8417ff
Fiorella Bruno <fiorellacbruno@gmail.com>
2024-08-25 23:21:16 -0300

    actualiza el script.js y mejora css

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-const textArea = document.querySelector(".text-area");
-const encriptacion = document.querySelector(".texto__desencriptado");
+const textArea = document.querySelector(".texto__encriptado");
+const cifrado = document.querySelector(".cifrado");
 
 
 function btnEncriptador(){
-    const textoEncriptado = encriptador(textArea.value);
-    encriptacion.value = textoEncriptado;
+    const textoEncriptado = encriptador(textArea.value)
+    cifrado.value = textoEncriptado;
+    textArea.value ="";
+
+    cifrado.style.backgroundImage = "none";
+
 }
 
 function encriptador(stringEncriptacion){
     let matrizCodificacion = [
+        ["a", "ai"],
         ["e", "enter"],
         ["i", "imes"],
-        ["a", "ai"],
         ["o", "ober"],
         ["u", "ufat"]
     ];
@@ -25,4 +29,39 @@ function encriptador(stringEncriptacion){
         
     }
     return stringEncriptacion;
+}
+
+function btnDesencriptador(){
+    const textoDesencriptado = desencriptador(textArea.value)
+    cifrado.value = textoDesencriptado;
+    textArea.value ="";
+
+
+}
+
+function desencriptador(stringDesencriptacion){
+    let matrizCodificacion = [
+        ["a", "ai"],
+        ["e", "enter"],
+        ["i", "imes"],
+        ["o", "ober"],
+        ["u", "ufat"]
+    ];
+    stringDesencriptacion=stringDesencriptacion.toLowerCase();
+
+    for (let i = matrizCodificacion.length - 1; i >= 0; i--) {
+        stringDesencriptacion = stringDesencriptacion.replaceAll(matrizCodificacion[i][1], matrizCodificacion[i][0]);
+    }
+    return stringDesencriptacion;
+}
+
+function copiarTexto() {
+    const texto = document.querySelector(".cifrado").value;
+    
+    // Usa la API del portapapeles
+    navigator.clipboard.writeText(texto).then(() => {
+        console.log('Texto copiado al portapapeles');
+    }).catch(err => {
+        console.error('Error al copiar al portapapeles: ', err);
+    });
 }
